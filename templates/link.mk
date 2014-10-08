984dcebdded99d50d7ba108da9314506ef635811
darealshinji <djcj@gmx.de>
2014-10-09 00:19:29 +0200

    delte link.mk, use dh_link to create links

--- templates/link.mk
+++
@@ -1,8 +0,0 @@
-include debian/confflags
-
-datadir = $(CURDIR)/debian/$(NAME)-data/usr/share/games/$(NAME)/
-libdir = $(CURDIR)/debian/$(NAME)/usr/lib/$(NAME)/
-
-all:
-	$(foreach FILE,$(shell ls $(datadir)$(NAME)_Data),\
-		ln -s /usr/share/games/$(NAME)/$(NAME)_Data/$(FILE) $(libdir)$(NAME)_Data/$(FILE) ;)
